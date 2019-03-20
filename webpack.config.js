80c9a17ec1ffde0f366c30df9902ff9d8fb3a996
Wayne Irwin <wayne.j.irwin@gmail.com>
2019-03-20 23:21:00 +1100

    Allow webpack to import core modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  node:{
+    fs: 'empty',
+
+  },
+
   cache: true,
   entry: {
     main: './client/src/main.js',
