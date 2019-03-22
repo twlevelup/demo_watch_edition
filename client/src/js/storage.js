706e762461015b26b07d3c1e8e2495e3aa13aede
Wayne Irwin <wayne.j.irwin@gmail.com>
2019-03-22 20:23:16 +1100

    [Wayne|Michelle] Fix NaN issues with counter

--- client/src/js/storage.js
+++ client/src/js/storage.js
@@ -1,4 +1,5 @@
 const StorageHub = require('watch-framework').StorageHub;
-
+const data = require('../../resources/contacts.json');
+StorageHub.setJSON(data);
 StorageHub.setData('hello', 'world');
 StorageHub.setData('count', 0)
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,6 @@
 require('./main.css');
 require('./js/storage');
 require('./../resources/contactInfo');
-const StorageHub = require('watch-framework').StorageHub;
-const data = require('../resources/contacts.json');
-StorageHub.setJSON(data);
-
 const App = require('watch-framework').App;
 
 const routes = require("./js/routes");
